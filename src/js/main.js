fc5c9d94e55357c3be75e5e3494cdb3d7bccdee3
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-03-10 21:49:28 +0100

    Add structure for js functions by adding comments

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+/* Fetch data from brewery API */
+
+/* Fetch data from state API */
+
+/* Function to handle state clicks */
+
+/* Function to display/update side panel with information */
+
+/* Function to handle hover effect on states, add/remove highlight class */
+
+/*
+Attach functions to map elements by eventlisteners
+click --> handle state click
+mouseover --> handleMouseOver (add class)
+mouseout --> handleMouseOut (remove class)
+*/
