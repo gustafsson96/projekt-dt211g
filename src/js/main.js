0666bb18ec8eb055bc374420cb6a08ebda760ee6
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-03-13 17:48:23 +0100

    Delete Leaflet map to test SVG map

--- src/js/main.js
+++ src/js/main.js
@@ -15,22 +15,3 @@ mouseover --> handleMouseOver (add class)
 mouseout --> handleMouseOut (remove class)
 */
 
-/* Leaflet TEST */
-
-document.addEventListener("DOMContentLoaded", () => {
-    // Create a map centered on the US
-    const map = L.map('map', {
-        center: [37.8, -96],
-        zoom: 3,
-        maxBounds: [
-            [24.396308, -125.0], // Southwest corner (bottom-left)
-            [49.384358, -66.93457] // Northeast corner (top-right)
-        ],
-        maxBoundsViscosity: 1.0
-    });
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-});
